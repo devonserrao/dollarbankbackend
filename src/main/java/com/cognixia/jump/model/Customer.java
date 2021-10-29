a32f3920d39336367d26a7afab79ddc2ab4df044
devonserrao <devonserrao101@gmail.com>
2021-10-29 12:38:23 -0500

    Added GeneratedType.Identity to ID column

--- src/main/java/com/cognixia/jump/model/Customer.java
+++ src/main/java/com/cognixia/jump/model/Customer.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -15,6 +17,7 @@ public class Customer implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String fullName;
