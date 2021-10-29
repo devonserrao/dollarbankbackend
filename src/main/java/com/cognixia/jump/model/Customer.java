df4bc0d35d057ec1bcfbaa5b8061967214fe4e98
devonserrao <devonserrao101@gmail.com>
2021-10-29 12:09:31 -0500

    Added Onetomany relationship with account - Modified fields and constructors and getters and setters to Customer

--- src/main/java/com/cognixia/jump/model/Customer.java
+++ src/main/java/com/cognixia/jump/model/Customer.java
@@ -1,5 +1,95 @@
 package com.cognixia.jump.model;
 
-public class Customer {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Customer implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Integer id;
+	
+	private String fullName;
+	
+	private String pin;
+	
+	private String username;
+	
+	private String password;
+	
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+	private List<Account> accounts;
+	
+	public Customer() {
+		this(-1, "N/A", "0000", "invalidUser", "kmdsdm", new ArrayList<Account>());
+	}
+
+	public Customer(Integer id, String fullName, String pin, String username, String password, List<Account> accounts) {
+		super();
+		this.id = id;
+		this.fullName = fullName;
+		this.pin = pin;
+		this.username = username;
+		this.password = password;
+		this.accounts = accounts;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Account> getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
+	}
+	
+	
+	
 }
