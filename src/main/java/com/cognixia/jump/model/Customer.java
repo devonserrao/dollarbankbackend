f156dd2c5e651d2fa6a7e9f9c9abf2f2901f556b
devonserrao <devonserrao101@gmail.com>
2021-10-29 12:27:45 -0500

    Modified the Customer model class

--- src/main/java/com/cognixia/jump/model/Customer.java
+++ src/main/java/com/cognixia/jump/model/Customer.java
@@ -28,9 +28,6 @@ public class Customer implements Serializable {
 	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
 	private List<Account> accounts;
 	
-	public Customer() {
-		this(-1, "N/A", "0000", "invalidUser", "kmdsdm", new ArrayList<Account>());
-	}
 
 	public Customer(Integer id, String fullName, String pin, String username, String password, List<Account> accounts) {
 		super();
@@ -82,13 +79,6 @@ public class Customer implements Serializable {
 		this.password = password;
 	}
 
-	public List<Account> getAccounts() {
-		return accounts;
-	}
-
-	public void setAccounts(List<Account> accounts) {
-		this.accounts = accounts;
-	}
 	
 	
 	
