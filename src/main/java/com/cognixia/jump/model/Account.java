0fb16d8008021fa325eaca724f8e98af533b00d0
devonserrao <devonserrao101@gmail.com>
2021-11-02 14:42:53 -0500

    Added more functions for Account model and repository

--- src/main/java/com/cognixia/jump/model/Account.java
+++ src/main/java/com/cognixia/jump/model/Account.java
@@ -1,5 +1,68 @@
 package com.cognixia.jump.model;
 
-public class Account {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Account implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private Double balance;
+	
+	@ManyToOne
+	@JoinColumn(name = "customer_id", referencedColumnName = "id")
+	private Customer customer;
+
+	public Account() {
+		this(-1, 0.0);
+	}
+	
+	public Account(Integer id, Double balance) {
+		super();
+		this.id = id;
+		this.balance = balance;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", balance=" + balance + ", customer=" + customer + "]";
+	}
+	
+	
+	
 }
