cd1faebc86f9ea2f269a3ec0eae8f3de1a5ef22b
devonserrao <devonserrao101@gmail.com>
2021-11-02 14:57:23 -0500

    Added APIs to CustomerController

--- src/main/java/com/cognixia/jump/controller/CustomerController.java
+++ src/main/java/com/cognixia/jump/controller/CustomerController.java
@@ -3,11 +3,15 @@ package com.cognixia.jump.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognixia.jump.model.Account;
 import com.cognixia.jump.model.Customer;
 import com.cognixia.jump.repository.CustomerRepository;
 
@@ -30,4 +34,18 @@ public class CustomerController {
 		
 		return new Customer();
 	}
+	
+	@PostMapping("/customer")
+	public ResponseEntity<?> addCustomer(@RequestBody Customer customer) {
+		customer.setId(-1);
+		
+		for(Account account : customer.getAccounts()) {
+			account.setId(-1);
+		}
+		
+		Customer addedCust = repo.save(customer);
+		
+		return ResponseEntity.status(201).body(addedCust);
+	}
+	
 }
