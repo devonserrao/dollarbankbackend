67a567f8540799136a6afa42e4be2e012492cc69
devonserrao <devonserrao101@gmail.com>
2021-11-02 14:52:35 -0500

    Added API requests for the customerController

--- src/main/java/com/cognixia/jump/controller/CustomerController.java
+++ src/main/java/com/cognixia/jump/controller/CustomerController.java
@@ -1,5 +1,33 @@
 package com.cognixia.jump.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognixia.jump.model.Customer;
+import com.cognixia.jump.repository.CustomerRepository;
+
+@RequestMapping("/api")
+@RestController
 public class CustomerController {
 
+	@Autowired
+	CustomerRepository repo;
+	
+	@GetMapping("/customer")
+	public List<Customer> getCustomers() {
+		return repo.findAll();
+	}
+	
+	@GetMapping("/customer/{id}")
+	public Customer getCustomer(@PathVariable int id) {
+		if(repo.existsById(id))
+			return repo.getById(id);
+		
+		return new Customer();
+	}
 }
