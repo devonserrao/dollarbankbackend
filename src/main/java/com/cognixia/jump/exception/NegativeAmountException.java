9dee3ef0777f14da552ef93bd29b03ed636a2abe
devonserrao <devonserrao101@gmail.com>
2021-11-14 22:39:20 -0600

    Added a Exception package with NegativeAmountException to be thrown when user inputs a negative amount for withdrawal, deposit or transferral of funds

---
+++ src/main/java/com/cognixia/jump/exception/NegativeAmountException.java
@@ -0,0 +1,15 @@
+package com.cognixia.jump.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NegativeAmountException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public NegativeAmountException(String message) {
+		super(message);
+	}
+	
+}
